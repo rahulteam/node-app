15cbdee325aa96cf3eab6ec9cc2b7e6c627ac543
bswsahoo11111 <32266851+bswsahoo11111@users.noreply.github.com>
2022-05-02 16:13:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Java Home App - version-155!!</h1> \n');
+  res.send('<h1 style="color:purple;">Java Home App - version-166655!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
